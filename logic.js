ec650fc01c07668882ff5c235a6dcdbaba91cc44
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-28 12:11:16 -0500

    set up html, and set associated vars for use in js

---
+++ logic.js
@@ -0,0 +1,21 @@
+/*############################################
+#    Global Varialbes
+##############################################*/
+// sidenav elements
+citySearchForm = $("#citySearchForm");
+cityInput = $("#cityInput");
+searchedCardGroup = $("#searchedCardGroup");
+
+//main content elements
+curWeatherCard = $("#curWeatherCard");
+dayForcastDiv = $("#dayForcast");
+
+
+/*############################################
+#    Functions
+##############################################*/
+
+
+/*############################################
+#    Main
+##############################################*/
