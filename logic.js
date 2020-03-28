9bd72ea21ac880de2c3a4732e503e83de8030209
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-28 14:56:57 -0500

    added uv index information and time/date of call to current weather

--- logic.js
+++ logic.js
@@ -12,9 +12,9 @@ var dayForcastDiv = $("#dayForcast");
 
 //OpenWeather
 var authKey = "6b1f3503a9686063d7fa4518108b400c";
-var dayforcastQRY = "https://api.openweathermap.org/data/2.5/forecast?appid="+authKey+"&q=";
+var dayforcastQry = "https://api.openweathermap.org/data/2.5/forecast?appid="+authKey+"&q=";
 var curWeatherQry = "https://api.openweathermap.org/data/2.5/weather?appid="+authKey+"&q=";
-var curWeatherQry = "https://api.openweathermap.org/data/2.5/weather?appid="+authKey+"&q=";
+var uvQry = "https://api.openweathermap.org/data/2.5/uvi?appid="+authKey;
 
 
 /*############################################
@@ -24,51 +24,61 @@ function kelvToF(temp){
     return ((temp - 273.15)*(9/5) + 32).toFixed(2);
 }
 
-function getIcon(weather){
-    
+function getIcon(weather){  
     weatherIcon = $("<i/>");
     //accounting for possibility of multiple weather icons, as described in the api documentation
     for (const elem of weather){
         weatherIcon.append( $("<img/>",{src:'http://openweathermap.org/img/wn/'+elem.icon+'@2x.png', alt:elem.description}))
-        console.log(weatherIcon);
     }
-
-
-    //Implementation using fontAwesome
-    // weatherIcon = $("<i/>");
-    // weatherIcon.addClass("ml-3")
-    // switch(weather){
-    //     case "Rain":
-    //         weatherIcon.addClass("fas fa-cloud-showers-heavy")
-    //         break;
-    //     default:
-    //         weatherIcon.addClass("far fa-question-circle")
-    //         break;
-    // }
-
     return weatherIcon;
 }
+
+
+function getUV(response){
+    //get the UV index via ajax call
+    $.ajax({url:uvQry+"&lat="+response.coord.lat+"&lon="+response.coord.lon, method:"GET"})
+    .then(function(uvresponse){
+        //console.log(uvresponse);
+        // scale index colors pulled from https://www.epa.gov/sunsafety/uv-index-scale-0
+        var uv = uvresponse.value;
+        var uvScale;
+
+        //set class name based on severity of UV
+        if(uv < 3 ){uvScale = "uvLow"}
+        else if(uv < 6 ){uvScale = "uvMod"}
+        else if(uv < 8 ){uvScale = "uvHi"}
+        else if(uv < 11 ){uvScale = "uvVH"}
+        else{uvScale = "uvEx"}
+        
+        //add index and appropriate class
+        $("#uvDiv").append( $("<span/>",{class:"uv "+uvScale, text:uv}) );
+    });
+}
+
 
 function printWeatherForCity(cityName){
     $.ajax({url:curWeatherQry+cityName, method:"GET"})
     .then(function(response){
-
         console.log(response);
+        var time = moment();
+
         //clear previous information
         curWeatherCard.empty();
 
-        // append data from JSON
+        // append data from JSON to DOM
         curWeatherCard.append([
-            $('<h3/>',{text:response.name}).append(
-                getIcon(response.weather)
-            ),
-            $('<div/>',{text:"Temperature: "+kelvToF(response.main.temp)+"°F"}),
-            $('<div/>',{text:"Humidity: "+response.main.humidity+"%"}),
-            $('<div/>',{text:"Wind Speed: "+response.wind.speed+" MPH"}),
-            $('<div/>',{text:"UV Index: "}).append(
-                $("<div/>",{class:"uv"})
-            )
+            $("<div/>",{class:"card-header text-center m-0 h-3", text:time.format('ddd MMMM Do YYYY, h:mm a')}),
+            $("<div/>",{class:"card-body"}).append([
+                $('<h3/>',{text:response.name}).append(
+                    getIcon(response.weather)
+                ),
+                $('<div/>',{text:"Temperature: "+kelvToF(response.main.temp)+"°F"}),
+                $('<div/>',{text:"Humidity: "+response.main.humidity+"%"}),
+                $('<div/>',{text:"Wind Speed: "+response.wind.speed+" MPH"}),
+                $('<div/>',{id:"uvDiv",text:"UV Index: "})
+            ])
         ])
+        getUV(response);
 
     });
 }
